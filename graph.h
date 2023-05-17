b6c7db6bac32f820f41a0541fcab8465edbf60d6
Matey Bogachev <boga4iov.matvei2016@yandex.ru>
2023-05-17 19:43:23 +0400

    add, find walk_in_deeper_and_maible

--- graph.h
+++ graph.h
@@ -1,42 +1,145 @@
 #pragma once
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
-template<typename Vertex, typename Distance = double>// Vertex - вершина  Distance - расто€ние
-class Graph {
-public:
-    struct Edge 
-    { 
-
-    }
-
-    struct 
-
-    //проверка-добавление-удаление вершин
-    bool has_vertex(const Vertex& v) const;
-    void add_vertex(const Vertex& v);
-    bool remove_vertex(const Vertex& v);
-    vector<Vertex> vertices() const;
-
-
-    //проверка-добавление-удаление ребер
-    void add_edge(const Vertex& from, const Vertex& to, const Distance& d);
-    bool remove_edge(const Vertex& from, const Vertex& to);
-    bool remove_edge(const Edge& e); //c учетом рассто€ни€
-    bool has_edge(const Vertex& from, const Vertex& to) const;
-    bool has_edge(const Edge& e); //c учетом рассто€ни€ в Edge
-
-    //получение всех ребер, выход€щих из вершины
-    vector<Edge> edges(const Vertex& vertex);
-
-    size_t order() const; //пор€док
-    size_t degree() const; //степень
-
-
-    //поиск кратчайшего пути
-    vector<Edge> shortest_path(const Vertex& from,const Vertex& to) const;
-    //обход
-    vector<Vertex> walk(const Vertex& start_vertex)const;
+enum Color {
+    WHITE, GRAY, BLACK
 };
 
+template<typename V = int, typename D = double>// Vertex - вершина  Distance - расто€ние
+class Graph {
+private:
+    struct Edge {
+        Edge* next;
+        V vertex;
+        D weight;
+        Edge(D weight, V vertex, Edge* next = nullptr) :weight(weight), vertex(vertex), next(next) {}
+    };
+    struct Vertex {
+        Edge* edges;
+        V name;
+        Color color;
+        Vertex(V name, Color color = WHITE, Edge* edges = nullptr) : name(name), color(color), edges(edges) {}
+    };
+    vector<Vertex> Verts;
+public:
+    ~Graph() {
+        Verts.clear();
+    }
+    bool find_E(V from , V to){
+        int id_from = find_index_V(from);
+        if (id_from == -1) { throw("¬ершины откуда нет"); }
+        int id_to = find_index_V(to);
+        if (id_to == -1) { throw("¬ершины куда нет"); }
+        Edge* tmp = Verts[id_from];
+        while (tmp != nullptr) {
+            if (tmp->vertex == Verts[id_to].name) {
+                return true;
+            }
+            tmp = tmp->next;
+        }
+        return false;
+    }
+    bool find_V(V name) {
+        for (int i = 0; i < Verts.size(); i++) {
+            if (Verts[i].name == name) {
+                return true;
+            }
+        }
+        return false;
+    }
+    int find_index_V(V name) {
+        for (int i = 0; i < Verts.size(); i++) {
+            if (Verts[i].name == name) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    bool add_V(V name){
+        if (find_V(name) == false) {
+            Verts.push_back(Vertex(name));
+            return true;
+        }
+        return false;
+    }
+    void delete_E(V from , V to){
+         
+    }
+
+
+
+    void delete_V(V name){
+        int id = find_index_V(name);
+        if (id == -1) {
+            throw("“акой вершины нет. тилт....");
+        }
+        for (int i=0; i < Verts.size(); i++) {
+            
+            if(find_E(Verts[id].name, Verts[i].name)) == true{
+                delete_E(Verts[id].name, Verts[i].name);
+            }
+
+            if (find_E(Verts[id].name, Verts[i].name)) == true{
+                delete_E(Verts[id].name, Verts[i].name);
+            }
+        }
+        Verts.erase(Verts.begin() + id);        
+    }
+
+
+
+
+    void add_E(V from, V to, D weight) {
+        int id_from = find_index_V(from);
+        int id_to = find_index_V(to);
+        if (id_to == -1) {
+            throw("¬ершины туда куда ведет ребро нет");
+        }
+        if (id_from == -1) {
+            throw("¬ершины, откуда ведет ребро, нет ");
+        }
+        Edge* new_edge = new Edge(weight, to);
+        if (Verts[id_from].edges == nullptr) {
+            Verts[id_from].edges = new_edge;
+            return;
+        }
+        Edge* tmp = Verts[id_from].edges;
+        while (tmp->next != nullptr) {
+            tmp = tmp->next;
+        }
+        tmp->next = new_edge;
+    }
+    void firma_gerasim_belim_krasim() {
+        for (int i = 0; i < Verts.size(); i++) {
+            Verts[i].color = WHITE;
+        }
+    }
+    void deep_cool(Vertex& current, vector<V>& result) {
+        current.color = GRAY;
+        result.push_back(current.name);
+        Edge* tmp = current.edges;
+        while (tmp != nullptr) {
+            int id = find_index_V(tmp->vertex);
+            if (Verts[id].color == WHITE) {
+                deep_cool(Verts[id], result);
+            }
+            tmp = tmp->next;
+        }
+        current.color = BLACK;
+    }
+
+    vector<V> walk_in_deep(V start) {
+        vector<V> result;
+        int id = find_index_V(start);
+        if (id == -1) {
+            throw"ќтправна€ вершина не найдена... наш корабль идет ко дну.. помоги мне € утону..";
+        }
+        deep_cool(Verts[id], result);
+        firma_gerasim_belim_krasim();
+        return result;
+    }
+
+};
--- main.cpp
+++ main.cpp
@@ -1,1 +1,25 @@
 #include "graph.h"
+#include <windows.h>
+#include <conio.h>
+using namespace std;
+
+int main() {
+	Graph<int> a;
+	a.add_V(1);
+	a.add_V(2);
+	a.add_V(3);
+	a.add_V(4);
+
+	a.add_E(2,1,10);
+	a.add_E(1,3,4);
+	a.add_E(2,3,5);
+	a.add_E(2, 4, 3);
+
+	a.delete_V(3); 
+	vector<int> vv= a.walk_in_deep(3);
+	for (auto i : vv) {
+		cout << i << " ";
+	}
+	_getch();
+	//a.showGraph();
+}
