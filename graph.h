d664313735d73ffe5823f1b11d2d7db50aa97626
Matey Bogachev <boga4iov.matvei2016@yandex.ru>
2023-05-20 11:16:42 +0400

    начало создания меню

--- graph.h
+++ graph.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -8,138 +9,73 @@ enum Color {
     WHITE, GRAY, BLACK
 };
 
-template<typename V = int, typename D = double>// Vertex - вершина  Distance - растояние
+template<typename V, typename D>
 class Graph {
 private:
     struct Edge {
-        Edge* next;
         V vertex;
         D weight;
-        Edge(D weight, V vertex, Edge* next = nullptr) :weight(weight), vertex(vertex), next(next) {}
+        Edge(V vertex, D weight) : vertex(vertex), weight(weight) {}
     };
     struct Vertex {
-        Edge* edges;
+        vector<Edge> edges;
         V name;
         Color color;
-        Vertex(V name, Color color = WHITE, Edge* edges = nullptr) : name(name), color(color), edges(edges) {}
+        Vertex(V name, Color color = WHITE) :name(name), color(color) {}
     };
-    vector<Vertex> Verts;
+    vector<Vertex> graph;
 public:
     ~Graph() {
-        Verts.clear();
+        graph.clear();
     }
-    bool find_E(V from , V to){
+    int size_g() { return graph.size(); }
+    bool find_E(V from, V to) {
         int id_from = find_index_V(from);
         if (id_from == -1) { throw("Вершины откуда нет"); }
         int id_to = find_index_V(to);
         if (id_to == -1) { throw("Вершины куда нет"); }
-        Edge* tmp = Verts[id_from];
-        while (tmp != nullptr) {
-            if (tmp->vertex == Verts[id_to].name) {
-                return true;
-            }
-            tmp = tmp->next;
-        }
-        return false;
-    }
-    bool find_V(V name) {
-        for (int i = 0; i < Verts.size(); i++) {
-            if (Verts[i].name == name) {
+        vector<Edge>& tmp = graph[id_from].edges;
+        for (int i = 0; i < tmp.size(); i++) {
+            if (tmp[i].vertex == to) {
                 return true;
             }
         }
         return false;
     }
     int find_index_V(V name) {
-        for (int i = 0; i < Verts.size(); i++) {
-            if (Verts[i].name == name) {
+        for (int i = 0; i < graph.size(); i++) {
+            if (graph[i].name == name) {
                 return i;
             }
         }
         return -1;
     }
-    bool add_V(V name){
-        if (find_V(name) == false) {
-            Verts.push_back(Vertex(name));
-            return true;
-        }
-        return false;
-    }
-    void delete_E(V from , V to){
-         
-    }
-
-
-
-    void delete_V(V name){
-        int id = find_index_V(name);
-        if (id == -1) {
-            throw("Такой вершины нет. тилт....");
-        }
-        for (int i=0; i < Verts.size(); i++) {
-            
-            if(find_E(Verts[id].name, Verts[i].name)) == true{
-                delete_E(Verts[id].name, Verts[i].name);
-            }
-
-            if (find_E(Verts[id].name, Verts[i].name)) == true{
-                delete_E(Verts[id].name, Verts[i].name);
-            }
-        }
-        Verts.erase(Verts.begin() + id);        
-    }
-
-
-
-
-    void add_E(V from, V to, D weight) {
-        int id_from = find_index_V(from);
-        int id_to = find_index_V(to);
-        if (id_to == -1) {
-            throw("Вершины туда куда ведет ребро нет");
-        }
-        if (id_from == -1) {
-            throw("Вершины, откуда ведет ребро, нет ");
-        }
-        Edge* new_edge = new Edge(weight, to);
-        if (Verts[id_from].edges == nullptr) {
-            Verts[id_from].edges = new_edge;
+    void add_V(V name) {
+        if (find_index_V(name) == -1) {
+            Vertex tmp = Vertex(name);
+            graph.push_back(tmp);
             return;
         }
-        Edge* tmp = Verts[id_from].edges;
-        while (tmp->next != nullptr) {
-            tmp = tmp->next;
-        }
-        tmp->next = new_edge;
+        throw("Такая вершина уже есть");
     }
-    void firma_gerasim_belim_krasim() {
-        for (int i = 0; i < Verts.size(); i++) {
-            Verts[i].color = WHITE;
-        }
+    bool add_E(V from, V to, D weight) {
+        int id_from = find_index_V(from);
+        if (id_from == -1) { throw("Вершины откуда нет"); }
+        int id_to = find_index_V(to);
+        if (id_to == -1) { throw("Вершины куда нет"); }
+        if (find_E(from, to) == true) { throw("Упс такое ребро уже есть, больше дороги строить не будем!"); }
+        graph[id_from].edges.push_back(Edge(to, weight));
     }
-    void deep_cool(Vertex& current, vector<V>& result) {
-        current.color = GRAY;
-        result.push_back(current.name);
-        Edge* tmp = current.edges;
-        while (tmp != nullptr) {
-            int id = find_index_V(tmp->vertex);
-            if (Verts[id].color == WHITE) {
-                deep_cool(Verts[id], result);
+    void delete_E(V from, V to) {
+        if (find_E(from, to) == false) { throw("Такого ребра нет"); }
+        int id_from = find_index_V(from);
+        vector<Edge>& tmp = graph[id_from].edges;
+        for (int i = 0; i < tmp.size(); i++) {
+            if (tmp[i].vertex == to) {
+                tmp.erase(tmp.begin() + i);
+                break;
             }
-            tmp = tmp->next;
         }
-        current.color = BLACK;
-    }
-
-    vector<V> walk_in_deep(V start) {
-        vector<V> result;
-        int id = find_index_V(start);
-        if (id == -1) {
-            throw"Отправная вершина не найдена... наш корабль идет ко дну.. помоги мне я утону..";
-        }
-        deep_cool(Verts[id], result);
-        firma_gerasim_belim_krasim();
-        return result;
     }
 
 };
--- main.cpp
+++ main.cpp
@@ -1,25 +1,200 @@
 #include "graph.h"
 #include <windows.h>
 #include <conio.h>
+#include <type_traits>
 using namespace std;
 
-int main() {
-	Graph<int> a;
-	a.add_V(1);
-	a.add_V(2);
-	a.add_V(3);
-	a.add_V(4);
 
-	a.add_E(2,1,10);
-	a.add_E(1,3,4);
-	a.add_E(2,3,5);
-	a.add_E(2, 4, 3);
+void Print(const string& text) {
+	CONSOLE_SCREEN_BUFFER_INFO bufferInfo;
+	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	GetConsoleScreenBufferInfo(consoleHandle, &bufferInfo);
 
-	a.delete_V(3); 
-	vector<int> vv= a.walk_in_deep(3);
-	for (auto i : vv) {
-		cout << i << " ";
+	int screenWidth = bufferInfo.srWindow.Right - bufferInfo.srWindow.Left + 1;
+	int textWidth = static_cast<int>(text.size());
+
+	int leftMargin = (screenWidth - textWidth) / 2;
+	cout << string(leftMargin, ' ') << text << endl;
+}
+
+template <typename T>
+T check()
+{
+	T value;
+
+	while (!(cin >> value) || (cin.peek() != '\n'))
+	{
+		cin.clear();
+		while (cin.get() != '\n')
+		{
+			system("cls");
+			Print( "\tВведите корректные данные: ");
+		}
 	}
+	return value;
+}
+template <typename T>
+struct is_valid_V : std::disjunction<
+	is_same<T, int>,
+	is_same<T, std::string>,
+	is_same<T, char>,
+	is_same<T, double>
+> {};
+
+template <typename T>
+struct is_valid_D : std::disjunction<
+	is_same<T, int>,
+	is_same<T, double>
+> {};
+void next() {
+	Print("\n\n\tНажмите любую кнопку, чтобы продолжить");
 	_getch();
-	//a.showGraph();
+}
+template <typename V>
+vector<V>  selection_pair() {
+	Print("\n\t\tВыбор пары вершин");
+	Print( "\n\tВведите имя вершины (откуда): ");
+	V v_from = check<V>();
+	Print ( "\n\tВведите имя вершины (куда): ");
+	V v_to = check<V>();
+	vector<V> result;
+	result.push_back(v_from);
+	result.push_back(v_to);
+	return result;
+}
+template <typename V, typename D>
+void Edd_vertex(Graph<V,D> &G) {
+	system("cls");
+	Print ("\n\tВведите имя вершины, которую хотете добавить: ");
+	V v = check<V>();
+	try {
+		G.add_V(v);
+		Print ("\t\tВершина успешно добавлена");
+	}
+	catch (const char* ex) {
+		Print(ex);
+	}
+	next();
+
+}
+
+
+template <typename V, typename D>
+typename enable_if<is_valid_V<V>::value&& is_valid_D<D>::value, void>::type
+Edd_edge(Graph<V,D> &G){
+	if (G.size_g() < 2) {
+		return;
+	}
+	system("cls");
+	Print("\n\tВведите вес ребра: ");
+	D d = check<D>();
+	vector<V> pair = selection_pair<V>();
+	try {
+		G.add_E(pair[0], pair[1], d);
+		Print("Ребро из вершины '" + to_string(pair[0]) +"' в вершину '" + to_string(pair[1]) + "' успешно проведено");
+	}
+	catch (const char* ex) {
+		Print(ex);
+	}
+	next();
+}
+
+int menu_1() {
+	while (true) {
+		system("cls");
+		Print( "\n\n\n\n\n");
+		Print( "Добавить вершину         - [ 1 ]");
+		Print( "Добавить ребро           - [ 2 ]");
+		Print( "Удалить вершину          - [ 3 ]");
+		Print( "Удалить ребро            - [ 4 ]");
+		Print( "Обход в глубину          - [ 5 ]");
+		Print( "Найти мин путь           - [ 6 ]");
+		Print( " Лучшее место для склада  - [ 7 ]\n");
+		Print( "Назад                    - [esc]");
+		int key = _getch();
+		if (key == 49 || key == 50 || key == 51 || key == 52 || key == 53 || key == 54 || key == 55 || key == 27) { return key; }
+	}
+}
+
+template <typename V, typename D>
+void main_menu() {
+	Graph<V, D> G;
+menu:
+	int key = menu_1();
+	switch (key) {
+	case 49:
+		Edd_vertex<V, D>(G);
+		goto menu;
+	case 50:
+		Edd_edge<V, D>(G);
+		goto menu;
+	case 51:
+		//delete_V<V, D>(G);
+		goto menu;
+	case 52:
+		//delete_D<V, D>(G);
+		goto menu;
+	case 53:
+		goto menu;
+	case 54:
+		goto menu;
+	case 55:
+		goto menu;
+	case 27:
+		return;
+
+	}
+}
+
+int selection_menu(){
+here:
+	while (true) {
+		system("cls");
+		Print ( "\n\n\n\n\n");
+		Print( "Выбрать тип данных вершина и вес ребра");
+		Print( "Ребро -  int,    вес - int:	 [ 1 ]");
+		Print("Ребро -  string, вес - int:	 [ 2 ]");
+		Print ("Ребро -  char,   вес - int:	 [ 3 ]");
+		Print ("Ребро -  double, вес - int:	 [ 4 ]");
+		Print ("  Ребро -  int,    вес - double:[ 5 ]");
+		Print ("  Ребро -  string, вес - double:[ 6 ]");
+		Print ("  Ребро -  char,   вес - double:[ 7 ]");
+		Print ("  Ребро -  double, вес - double:[ 8 ]");
+
+		Print( "Выход - [esc]");
+		int key = _getch();
+		switch (key)
+		{
+		case(49):
+			main_menu<int, int>();
+			goto here;
+		case(50):
+			main_menu<string, int>();
+		case(51):
+			main_menu<char, int>();
+			goto here;
+		case(52):
+			main_menu<double, int>();
+			goto here;
+		case(53):
+			main_menu<int, double>();
+			goto here;
+		case(54):
+			main_menu<string, double>();
+			goto here;
+		case(55):
+			main_menu<char, double>();
+			goto here;
+		case(56):
+			main_menu<double, double>();
+			goto here;
+		case(27):
+			return -1;
+		}
+	}
+}
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	selection_menu();
 }
